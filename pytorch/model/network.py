52c6f7eebc6211ccd55b168e8b7076d5b8984cd6
Guillem Delgado Gonzalo <32647701+guillemdelgado@users.noreply.github.com>
2020-05-30 11:29:11 +0200

    Changed from efficientnet v0 to v7

--- pytorch/model/network.py
+++ pytorch/model/network.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.model = EfficientNet.from_pretrained('efficientnet-b0')
+        self.model = EfficientNet.from_pretrained('efficientnet-b7')
         # 1280 is the number of neurons in last layer. is diff for diff. architecture
         self.dense_output = nn.Linear(1280, num_classes)
 
