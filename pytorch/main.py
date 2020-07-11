0ce7b2d98847684b203a39adbc832a7e5159f1d7
DELGADO GONZALO Ricard <rdg@csem.ch>
2020-07-11 11:39:01 +0200

    Removed annoying content

--- pytorch/main.py
+++ pytorch/main.py
@@ -90,11 +90,11 @@ val_loader = torch.utils.data.DataLoader(
 
 if nfolds == 0:
     val_loader = None
-from torchsummary import summary
+#from torchsummary import summary
 model = Net(num_classes=nclasses, config=config)
 
 fitter = Fitter(model=model, device=device, config=config)
-summary(fitter.model, (3, 512, 512))
+#summary(fitter.model, (3, 512, 512))
 fitter.fit(train_loader, val_loader)
 
 
